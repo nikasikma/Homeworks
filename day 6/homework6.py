51c7522face6302be3ef5cef4f23b82d100939bf
nikasikma <nikasiymashvili920@gmail.com>
2024-03-13 11:10:01 -0700

    commit

---
+++ day 6/homework6.py
@@ -0,0 +1,30 @@
+name = input("enter your name")
+surname = input("enter your surname")
+age = input("enter your age")
+place = input("enter place where you live")
+profesion = input("enter your profesion")
+hob = input("enter your hob")
+school_subject = input("enter your favourite subject")
+print(name,surname,'is', age,'years old', name,surname,'lives in',place,name,surname,'is',profesion,name,surname,'hob is',hob,name,surname,'favourite subject is',school_subject)
+
+
+
+
+num1=input("enter first number")
+num2=input("enter second number")
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+
+
+
+name = input("qeti enter your age ")
+name1 = input("romeo eter your age ")
+name2 = input("nika enter your age")
+print(name + 25 )
+print(name1 + 25)
+print(name2 + 25)
+            
+            
