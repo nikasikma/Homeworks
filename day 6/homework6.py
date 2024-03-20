b7a944f73a8fca2219b69d141287e1c8acb64329
nikasikma <nikasiymashvili920@gmail.com>
2024-03-20 03:04:12 -0700

    commit

--- day 6/homework6.py
+++ day 6/homework6.py
@@ -1,30 +1,31 @@
-name = input("enter your name")
-surname = input("enter your surname")
-age = input("enter your age")
-place = input("enter place where you live")
-profesion = input("enter your profesion")
-hob = input("enter your hob")
-school_subject = input("enter your favourite subject")
-print(name,surname,'is', age,'years old', name,surname,'lives in',place,name,surname,'is',profesion,name,surname,'hob is',hob,name,surname,'favourite subject is',school_subject)
+#მომხმარებელს შემოატანინეთ თავისი სახელი, გვარი, ასაკი, საცხოვრებელი ადგილი, პროფესია, ჰობი და კიდევ დამატებითი ინფორმაცია, შემდეგ ამ შემოტანილი ინფორმაციით ააწყვეთ ერთი დიდი წინადადება.
+# name = input("enter your name: ")
+# surname = input("enter your surname: ")
+# age = input("enter your age: ")
+# place = input("enter place where you live: ")
+# profesion = input("enter your profesion: ")
+# hob = input("enter your hob: ")
+# school_subject = input("enter your favourite subject: ")
+# print(name,surname,'is', age,'years old', name,surname,'lives in',place,name,surname,'is',profesion,name,surname,'hob is',hob,name,surname,'favourite subject is',school_subject)
 
 
 
+#მომხმარებელს შემოატანინეთ ორი სხვადასხვა რიცხვი და ამ შემოტანილ რიცხვებზე მოახდინეთ მათემათიკური ოპერაციები, როგორიცაა +,-,*,/
+# num1 = int(input("enter first number: "))
+# num2 = int(input("enter second number: "))
 
-num1=input("enter first number")
-num2=input("enter second number")
-
-print(num1+num2)
-print(num1-num2)
-print(num1*num2)
-print(num1/num2)
+# print(num1+num2)
+# print(num1-num2)
+# print(num1*num2)
+# print(num1/num2)
 
 
-
-name = input("qeti enter your age ")
-name1 = input("romeo eter your age ")
-name2 = input("nika enter your age")
-print(name + 25 )
-print(name1 + 25)
-print(name2 + 25)
+#შემოიტანეთ თქვენი ოჯახის წევრების ასაკი და დაუპრინტეთ თუ რამდენი წლისები იქნებიან 25 წლის შემდეგ.
+# name = int(input("qeti enter your age:  "))
+# name1 = int(input("romeo eter your age:  "))
+# name2 = int(input("nika enter your age: "))
+# print(name + 25 )
+# print(name1 + 25)
+# print(name2 + 25)
             
             
---
+++ day 7/day 7 homework.py
@@ -0,0 +1,9 @@
+str = input("enter any string")
+inte = int (input("enter any number"))
+flo = float(int(input("enter any number")))
+
+
+input(12+8)
+input(9-7)
+input(4*7)
+input(8/4)
